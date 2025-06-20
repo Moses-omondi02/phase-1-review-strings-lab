e6b4e5d66043ba61d466cd176a1d2362dc450c1b
erick moses <moseserickomondi@gmail.com>
2025-06-20 11:16:04 +0300

    update the new changes on index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser ='Erick Moses';
+const welcomeMessage ="Welcome to Flatbook, Erick Moses!"
+const excitedWelcomeMessage ="WELCOME TO FLATBOOK, ERICK MOSES!"
+const shortGreeting ='Welcome, E!'
